0112aabb94c410482b7241e0447e2bd6851a028e
Laraib Tabbassum <161297553+laraib28@users.noreply.github.com>
2024-04-19 21:15:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+console.log(answer);
+
+//conditional statement//
+if (answer.operators==="Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if (answer.operators==="Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+  } else if (answer.operators==="Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+    } else if (answer.operators==="Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+     } else {
+        console.log("Please select valid operator");
+     }
